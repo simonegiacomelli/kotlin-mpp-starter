ef0a330590d1c8dfd9b989a90ed0efe5c0c79266
Simone Giacomelli <simone.giacomelli@gmail.com>
2022-04-17 08:19:03 +0200

    offcanvas development

---
+++ prj-website/src/jsMain/kotlin/pages/bootstrap/OffcanvasDemoWidget.kt
@@ -0,0 +1,53 @@
+package pages.bootstrap
+
+import org.w3c.dom.HTMLElement
+import widget.Widget
+
+class OffcanvasDemoWidget : Widget(//language=HTML
+    """
+<button id="btnToggle" style="display: none" class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling"
+        aria-controls="offcanvasScrolling">Enable body scrolling
+</button>
+
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1"
+     id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Colored with scrolling</h5>
+        <span>ciao span</span>
+        <input>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <p>Try scrolling the rest of the page to see this option in action.</p>
+        <ol class="list-group list-group-numbered">
+            <li class="list-group-item d-flex justify-content-between align-items-start">
+                <div class="ms-2 me-auto">
+                    <div class="fw-bold">Subheading</div>
+                    Content for list item
+                </div>
+                <span class="badge bg-primary rounded-pill">14</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-start">
+                <div class="ms-2 me-auto">
+                    <div class="fw-bold">Subheading</div>
+                    Content for list item
+                </div>
+                <span class="badge bg-primary rounded-pill">14</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-start">
+                <div class="ms-2 me-auto">
+                    <div class="fw-bold">Subheading</div>
+                    Content for list item
+                </div>
+                <span class="badge bg-primary rounded-pill">14</span>
+            </li>
+        </ol>
+    </div>
+</div>
+
+
+"""
+) {
+    private val btnToggle: HTMLElement by this
+    fun toggle() = btnToggle.click()
+}
