7542fa6f2216a8c64dd08dc143db8392ace8344b
Simone Giacomelli <simone.giacomelli@gmail.com>
2022-04-17 20:22:47 +0200

    better fetch error handling

--- prj-common/src/jsMain/kotlin/rpc/RpcClient.kt
+++ prj-common/src/jsMain/kotlin/rpc/RpcClient.kt
@@ -9,9 +9,16 @@ import state.ClientState
 
 suspend fun ClientState.apiDispatcher(apiName: String, payload: String): String {
     val httpRequest = RpcRequest(RpcMessage(apiName, payload), session_id).toHttpRequest(ApiBaseUrl)
-    val result = doFetch(httpRequest).toRpcResponse().result
+    val doFetch = runCatching { doFetch(httpRequest) }
+    if (doFetch.isFailure) {
+        val exception = doFetch.exceptionOrNull()!!
+        toast("Errore di comunicazione col server (fetch)")
+        throw exception
+    }
+    val result = doFetch.getOrThrow().toRpcResponse().result
     if (result.isFailure) {
         val exception = result.exceptionOrNull()!! as HttpResponseException
+        toast("Errore di comunicazione col server (${exception.httpResponse.status})")
         println("error http status=" + exception.httpResponse.status)
         println(exception.httpResponse.body)
         throw exception
@@ -26,7 +33,6 @@ private suspend fun ClientState.doFetch(httpRequest: HttpRequest): HttpResponse 
     request.headers = Headers().also { headers.forEach { entry -> it.append(entry.key, entry.value) } }
     val urlWithParams = url + "?" + parameters.map { it.key + "=" + it.value }.joinToString("&")
     val resp = window.fetch(urlWithParams, request).await()
-    if (!resp.ok) toast("Errore di comunicazione col server")
     val text = resp.text().await()
     // resp.headers() is present but the kotlin interface lacks the method to enumerate them so:
     return HttpResponse(text, emptyMap(), resp.status.toInt(), null)
