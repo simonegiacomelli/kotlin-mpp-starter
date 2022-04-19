fc6fed1ce662a191b265db996f116874685885df
Simone Giacomelli <simone.giacomelli@gmail.com>
2022-04-19 17:57:59 +0200

    works on jvm but not on js

--- prj-common/src/commonTest/kotlin/databinding/BindableTest.kt
+++ prj-common/src/commonTest/kotlin/databinding/BindableTest.kt
@@ -1,17 +1,27 @@
 package databinding
 
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class BindableTest {
 
+    @kotlinx.serialization.Serializable
+    class User : Bindable() {
+        var name: String by this()
+        var age: Int by this()
+    }
+
     @Test
     fun test_serialization() {
-//        val original = User().apply { name = "foo"; age = 42 }
-//        val str = Json.encodeToString(original)
-//        println("serialized instance: `$str`")
-//        val deserialized = Json.decodeFromString<User>(str)
-//        assertEquals("foo", deserialized.name)
-//        assertEquals(42, deserialized.age)
+        val original = User().apply { name = "foo"; age = 42 }
+        val str = Json.encodeToString(original)
+        println("serialized instance: `$str`")
+        val deserialized = Json.decodeFromString<User>(str)
+        assertEquals("foo", deserialized.name)
+        assertEquals(42, deserialized.age)
     }
 
 }
--- prj-common/src/commonTest/kotlin/databinding/User.kt
+++
@@ -1,7 +0,0 @@
-package databinding
-
-//@Serializable
-class User : Bindable() {
-    var name: String by this()
-    var age: Int by this()
-}
