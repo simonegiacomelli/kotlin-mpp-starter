b06742cace01fadc06a28c1cbf333d7fa05c2b0e
Simone Giacomelli <simone.giacomelli@gmail.com>
2022-04-19 12:40:00 +0200

    poc for forum

--- prj-common/src/jvmMain/kotlin/discuss/kotlinlang/rpc_poc/send.kt
+++ prj-common/src/jvmMain/kotlin/discuss/kotlinlang/rpc_poc/send.kt
@@ -1,54 +1,48 @@
-@file:Suppress("UNCHECKED_CAST")
+@file:Suppress("UNCHECKED_CAST", "UNUSED_ANONYMOUS_PARAMETER")
 @file:OptIn(ExperimentalSerializationApi::class)
+/* @formatter:off */
 
 package discuss.kotlinlang.rpc_poc
 
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import kotlin.reflect.KClass
 
-interface Request<Res : Any>
+inline fun <reified Req : Request<Res>, reified Res : Any> Req.send(): Res = TODO()
+inline fun <reified Req : Request<Res>, reified Res : Any> addHandler(noinline handler: (Req) -> Res): Unit = TODO()
 
-suspend inline fun <reified Req : Request<Res>, reified Res : Any> Req.send(): Res = TODO()
+interface Request<in Res : Any>
+@Serializable data class SumRequest(val a: Int, val b: Int) : Request<SumResponse>
+@Serializable data class SumResponse(val sum: Int)
 
-data class ReifiedSerializer(val serializer: (Any) -> String, val deserializer: (String) -> Any)
-data class TwinSerializer(val requestSerializer: ReifiedSerializer, val responseSerializer: ReifiedSerializer)
+object IssueRequest {
 
-inline fun <reified Req, reified Res> twinSerializer() = TwinSerializer(serializers<Req>(), serializers<Res>())
-
-inline fun <reified T> serializers(): ReifiedSerializer = ReifiedSerializer(
-    serializer = { instance: T -> Json.encodeToString(instance) } as (Any) -> String,
-    deserializer = { json: String -> Json.decodeFromString<T>(json) } as (String) -> Any
-)
-
-fun nameOf(klass: KClass<*>): String = klass.simpleName ?: error("simpleName not available")
-
-class Handler(val name: String, val handler: (Any) -> Any, val TwinSerializer: TwinSerializer)
-
-class Handlers {
-    val handlers = mutableMapOf<String, Handler>()
-    inline fun <reified Req : Request<Res>, reified Res : Any> registerHandler(
-        noinline handler: (Req) -> Res,
-    ) {
-        val name = nameOf(Req::class)
-        if (handlers.containsKey(name)) error("Handler named `$name` already registerd")
-        handlers[name] = Handler(name, handler as (Any) -> Any, twinSerializer<Req, Res>())
+    @JvmStatic
+    fun main(args: Array<String>) {
+        // this is ok: the type of send() is SumResponse, as expected
+        println(SumRequest(30, 12).send().sum)
     }
 }
 
-@Serializable
-data class SumRequest(val a: Int, val b: Int) : Request<SumResponse>
+object HandleRequest {
 
-@Serializable
-data class SumResponse(val sum: Int)
+    @JvmStatic
+    fun main(args: Array<String>) {
 
-fun main() {
-    val handlers = Handlers()
-    handlers.registerHandler { request: SumRequest -> SumResponse(request.a + request.b) }
-    handlers.registerHandler { request: SumRequest -> Any() }
-//    handlers.registerHandler<SumRequest, SumResponse> { request: SumRequest -> Any() }
-    handlers.registerHandler<SumRequest, SumResponse> { request: SumRequest -> SumResponse(request.a + request.b) }
+        // as I usually use it
+        addHandler { request: SumRequest -> SumResponse(request.a + request.b) }
+
+        // ok, no news here
+        addHandler<SumRequest, SumResponse> { request: SumRequest -> SumResponse(request.a + request.b) }
+
+        // this is the issue: I would expect a compiler error because Any() is not
+        // coherent with declaration above SumRequest(...) : Request<SumResponse>
+        addHandler { request: SumRequest -> Any() }
+
+        // this does not compile with error: Type mismatch: inferred type is Any but SumResponse was expected
+        // so, why is it that the previous invocation is fine with the compiler?!
+        /*
+        addHandler<SumRequest, SumResponse> { request: SumRequest -> Any() }
+        */
+    }
 }
+
