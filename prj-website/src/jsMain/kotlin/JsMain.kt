8a97d6c986d21f816b74c7bfa04eba11f0b29b84
Simone Giacomelli <simone.giacomelli@gmail.com>
2022-04-25 08:14:15 +0200

    fix ws url

--- prj-website/src/jsMain/kotlin/JsMain.kt
+++ prj-website/src/jsMain/kotlin/JsMain.kt
@@ -1,11 +1,19 @@
+import kotlinx.browser.document
+import kotlinx.browser.window
 import kotlinx.datetime.Clock
 import state.startupApplication
 
-const val version = "v0.1.5"
+const val version = "v0.1.6"
 
 suspend fun main() {
     println("ok $version " + (Clock.System.now()))
+    println(window.location.href)
+    console.log(document.currentScript)
     startupApplication()
-    OnewayApi.openWebSocket()
+
+    val l = window.location
+    val u = l.protocol.replace("http", "ws") + l.host + l.pathname + "ws1"
+    OnewayApi.openWebSocket(u)
+
 }
 
