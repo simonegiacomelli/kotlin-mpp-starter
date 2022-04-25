92a82a35f7c6dea55636f9c701b2be4a6937032d
Simone Giacomelli <simone.giacomelli@gmail.com>
2022-04-25 09:31:35 +0200

    removed useless indirection

--- prj-website/src/jsMain/kotlin/databinding/BooleanBridgeNN.kt
+++ prj-website/src/jsMain/kotlin/databinding/BooleanBridgeNN.kt
@@ -1,10 +1,9 @@
 package databinding
 
 import org.w3c.dom.HTMLInputElement
-import utils.forward
 
 class BooleanBridgeNN(override val target: HTMLInputElement) : PropertyBridge<Boolean>, HtmlElementObservable {
-    override var value by forward { target::checked }
+    override var value by target::checked
 }
 
 
