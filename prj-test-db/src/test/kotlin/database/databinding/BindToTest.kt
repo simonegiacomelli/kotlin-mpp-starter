f6349194284a9943190de71733265183bdf2b904
Simone Giacomelli <simone.giacomelli@gmail.com>
2022-04-29 10:50:24 +0200

    bind mapAll

--- prj-test-db/src/test/kotlin/database/databinding/BindToTest.kt
+++ prj-test-db/src/test/kotlin/database/databinding/BindToTest.kt
@@ -1,7 +1,6 @@
 package database.databinding
 
 import database.exposed.DatabaseTest
-import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Table
@@ -25,6 +24,7 @@ class BindToTest : DatabaseTest() {
         override val primaryKey = PrimaryKey(id)
     }
 
+    @Serializable
     data class User(
         var id: Int = 0,
         var name: String = "",
@@ -33,22 +33,25 @@ class BindToTest : DatabaseTest() {
 
     @Test
     fun test_bind() {
-        transaction {
-            db_usersInsert(1, "foo", 42)
-            db_usersInsert(2, "bar", 43)
-        }
-        val mapper = listOf(
+        db_users_Insert()
+
+        val target = listOf(
             db_users.id bindTo User::id,
             db_users.name bindTo User::name,
             db_users.counter bindTo User::counter
         ).toMapper { User() }
-        val users = transaction { db_users.selectAll().map { mapper.map(it) } }
-            .sortedBy { it.id }
 
-        assertEquals(listOf(User(1, "foo", 42), User(2, "bar", 43)), users)
+        db_users_asserts(target)
     }
 
-    private fun db_usersInsert(i: Int, s: String, i1: Int) {
+    @Test
+    fun test_bind_mapAll() {
+        db_users_Insert()
+        val target = exposedMapper { User() }.mapAll(db_users)
+        db_users_asserts(target)
+    }
+
+    private fun db_users_Insert(i: Int, s: String, i1: Int) = transaction {
         db_users.insert {
             it[id] = i
             it[name] = s
@@ -67,17 +70,18 @@ class BindToTest : DatabaseTest() {
         val fooId = db_users_autopk_Insert("foo", 42)
         val barId = db_users_autopk_Insert("bar", 43)
 
-        val mapper = listOf(
+        val target = listOf(
             db_users_autopk.id bindTo User::id,
             db_users_autopk.name bindTo User::name,
             db_users_autopk.counter bindTo User::counter
         ).toMapper { User() }
-        val users = transaction { db_users_autopk.selectAll().map { mapper.map(it) } }
-            .sortedBy { it.id }
 
-        assertEquals(listOf(User(fooId, "foo", 42), User(barId, "bar", 43)), users)
-
+        transaction {
+            val users = db_users_autopk.selectAll().map { target.map(it) }.sortedBy { it.id }
+            assertEquals(listOf(User(fooId, "foo", 42), User(barId, "bar", 43)), users)
+        }
     }
+
 
     private fun db_users_autopk_Insert(s: String, i: Int) = transaction {
         (db_users_autopk.insert {
@@ -86,22 +90,16 @@ class BindToTest : DatabaseTest() {
         } get (db_users_autopk.id)).value
     }
 
-    @Test
-    fun test_bindToSerializable() {
-        @Serializable
-        data class SUser(
-            var id: Int = 0,
-            var name: String = "",
-            var counter: Int = 0,
-        )
 
-//        exposedMapper { SUser() }.mapAll(db_users)
+    private fun db_users_asserts(target: ColumnsMapper<User>) {
+        transaction {
+            val users = db_users.selectAll().map { target.map(it) }.sortedBy { it.id }
+            assertEquals(listOf(User(1, "foo", 42), User(2, "bar", 43)), users)
+        }
+    }
+
+    private fun db_users_Insert() {
+        db_users_Insert(1, "foo", 42)
+        db_users_Insert(2, "bar", 43)
     }
 }
-
-@OptIn(InternalSerializationApi::class)
-inline fun <reified T : Any> exposedMapper(constructor: () -> T) {
-//    val s = T::class.
-//    s.descriptor.elementNames
-}
-
--- prj-website/src/jvmMain/kotlin/database/databinding/BindTo.kt
+++ prj-website/src/jvmMain/kotlin/database/databinding/BindTo.kt
@@ -4,7 +4,10 @@ package database.databinding
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.Table
 import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
 
 interface ColumnBind<E, V> {
     fun databaseToElement(element: E, resultRow: ResultRow)
@@ -14,6 +17,7 @@ interface ColumnBind<E, V> {
 interface ColumnsMapper<E> {
     fun map(resultRow: ResultRow): E
     fun columns(): List<Column<*>>
+    fun mapAll(table: Table): ColumnsMapper<E> = TODO()
 }
 
 infix fun <E, V> Column<V>.bindTo(property: KMutableProperty1<E, V>): ColumnBind<E, V> {
@@ -54,3 +58,23 @@ fun <E> Collection<ColumnBind<E, *>>.toMapper(new: () -> E): ColumnsMapper<E> {
         override fun columns(): List<Column<*>> = cb.map { it.column }
     }
 }
+
+
+inline fun <reified E : Any> exposedMapper(noinline constructor: () -> E): ColumnsMapper<E> {
+    return object : ColumnsMapper<E> {
+        override fun map(resultRow: ResultRow): E = constructor()
+        override fun columns(): List<Column<*>> = emptyList()
+        override fun mapAll(table: Table): ColumnsMapper<E> =
+            E::class.memberProperties.map { columnBind(it, table) }.toMapper(constructor)
+
+    }
+}
+
+fun <E> columnBind(property: KProperty1<E, *>, table: Table): ColumnBind<E, *> {
+    val column = table.columns.first { col -> col.name == property.name }
+    val col = column as Column<Any?>
+    val prop = property as KMutableProperty1<E, Any?>
+    return col.bindTo(prop)
+}
+
+
