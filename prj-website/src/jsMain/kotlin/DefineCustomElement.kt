945bfb743ff679f4bca46625db2d5be4c6e40168
Simone <simone.giacomelli@gmail.com>
2024-03-30 22:20:42 +0000

    better dx

--- prj-website/src/jsMain/kotlin/DefineCustomElement.kt
+++ prj-website/src/jsMain/kotlin/DefineCustomElement.kt
@@ -1,17 +1,17 @@
 import kotlinx.browser.document
 import kotlinx.browser.window
 import org.w3c.dom.HTMLElement
-import kotlin.reflect.KClass
 
 fun defineCustomElemenTest() {
-    defineCustomElement("my-element2", MyElement2::class) { MyElement2(it) }
+    defineCustomElement("my-element2") { MyElement2() }
     document.body?.append(document.createElement("my-element2"))
     console.log("defineCustomElemenTest")
 }
 
 
-class MyElement2(element: HTMLElement) : AbsCustomElement(element) {
-    init {
+class MyElement2 : AbsCustomElement() {
+
+    override fun constr() {
         console.log("constructor MyElement2 - kotlin")
 //        element.attachShadow(json("mode" to "open"))
         element.shadowRoot.asDynamic().innerHTML = "<h1>Hello World2</h1>"
@@ -34,11 +34,7 @@ class MyElement2(element: HTMLElement) : AbsCustomElement(element) {
     }
 }
 
-inline fun <reified T : AbsCustomElement> defineCustomElement(
-    tagName: String,
-    clazz: KClass<T>,
-    noinline constr: (HTMLElement) -> T
-) {
+inline fun <reified T : AbsCustomElement> defineCustomElement(tagName: String, noinline constr: () -> T) {
     val className = T::class.simpleName
     window.asDynamic()["kotlin_constructor_$className"] = constr
     val code = _customElement.replace("#ClassName", className!!).replace("#tagName", tagName)
@@ -53,7 +49,9 @@ val _customElement = """
             super();
             this.attachShadow({ mode: "open" });
             console.log('constructor #ClassName - javascript');
-            this._kt = window.kotlin_constructor_#ClassName(this);
+            this._kt = window.kotlin_constructor_#ClassName();
+            this._kt._element = this;
+            this._kt.constr();
             console.log(this._kt);
         }
 
@@ -80,7 +78,11 @@ val _customElement = """
 """
 
 @JsExport
-open class AbsCustomElement(val element: HTMLElement) {
+open class AbsCustomElement {
+    var _element: HTMLElement? = null
+    val element: HTMLElement get() = _element ?: error("element not set")
+
+    open fun constr() {}
 
     open fun connectedCallback() {}
 
